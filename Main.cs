4af8372d95dd1e1c5adfe6562dc01d8e9875d197
Manuel Mayer <suchbyte@hotmail.com>
2022-01-30 16:16:18 +0100

    Fix: crash when opening the action configurator

--- Main.cs
+++ Main.cs
@@ -18,6 +18,7 @@ namespace SuchByte.GHUBBatteries
 
         public override void Enable()
         {
+            this.Actions = new System.Collections.Generic.List<PluginAction>(); // Just create a empty list because otherwise it causes errors in Macro Deck
             GHubReader.Initilize();
         }
 
